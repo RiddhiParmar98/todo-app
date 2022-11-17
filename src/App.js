e7d1fa012f27bcc2203a8011137c2fe469260c02
riddhiparmar98 <riddhipr@itpathsolutions.co.in>
2022-11-17 11:13:24 +0530

    routes functionality added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ import { Container } from "@mui/material";
 import TodoList from "./Pages/todo/TodoList";
 import Login from "./Pages/Login";
 import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.min.css';
+import "react-toastify/dist/ReactToastify.min.css";
+import { Route, Routes } from "react-router-dom";
+
+
+const publicRoutesOptions = [
+  { component: <Login />, path: "/" },
+  { component: <TodoForm />, path: "/addtodo" },
+  { component: <TodoList />, path: "/todolist" },
+  { component: <TodoForm />, path: "/edittodo:id" },
+];
 
 function App() {
   return (
     <div>
-      <Container>
-    <Login />
-        <TodoForm />
-        <TodoList />
-      </Container>
-    <ToastContainer autoClose={500} />
+      <Routes>
+        {publicRoutesOptions.map(({ path, component }, idx) => (
+          <Route key={idx} exact {...{ path, element: component }} />
+        ))}
+      </Routes>
+      <ToastContainer autoClose={500} />
     </div>
   );
 }
